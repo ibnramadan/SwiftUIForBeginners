8db02653664e10b9d0b41143309676ee26f1374c
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2024-05-29 16:02:26 +0300

    Add existing project files prior to the push to GitHub.

---
+++ SwiftUIForBeginners/SwiftUIForBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIForBeginnersApp.swift
+//  SwiftUIForBeginners
+//
+//  Created by mohamed ramadan on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIForBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
