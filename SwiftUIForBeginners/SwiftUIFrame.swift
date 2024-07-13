fe288a662372ba5e6f90d14af0acb20542d8308e
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2024-07-13 20:48:19 +0300

    Episode17 code has been added.

---
+++ SwiftUIForBeginners/SwiftUIFrame.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIFrame.swift
+//  SwiftUIForBeginners
+//
+//  Created by mohamed ramadan on 13/07/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIFrame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIFrame()
+}
