316036ca78381f69756169850c6e5331b7355876
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2024-09-13 17:52:17 +0300

    .

--- SwiftUIForBeginners/SwiftUISlider.swift
+++ SwiftUIForBeginners/SwiftUISlider.swift
@@ -8,16 +8,7 @@
 import SwiftUI
 
 struct SwiftUISlider: View {
-    @State private var sliderValue: Double = 100
-    var body: some View {
-        VStack {
-            Text("Slider Value: \(sliderValue)")
-//            Slider(value: $sliderValue)
-//                .tint(.red)
-            
-//            Slider(value: $sliderValue, in: 0...100, step: 5)
-            
-        }.padding()
+
     }
 }
 
