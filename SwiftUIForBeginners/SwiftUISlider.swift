8aea09a44078a97900e56cf7806df8c61ca71655
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2024-09-13 17:51:29 +0300

    .

--- SwiftUIForBeginners/SwiftUISlider.swift
+++ SwiftUIForBeginners/SwiftUISlider.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct SwiftUISlider: View {
     @State private var sliderValue: Double = 100
-    @State private var sliderValueChanged: Bool = false
     var body: some View {
         VStack {
             Text("Slider Value: \(sliderValue)")
@@ -18,27 +17,6 @@ struct SwiftUISlider: View {
             
 //            Slider(value: $sliderValue, in: 0...100, step: 5)
             
-            
-            Slider(value: $sliderValue,
-                   in: 100...500,
-                   step: 100) {
-                Text("Slider")
-            } minimumValueLabel: {
-                Text("100")
-            } maximumValueLabel: {
-                Text("500")
-            } onEditingChanged: { sliderChanged in
-                print("\(sliderChanged)")
-                sliderValueChanged = sliderChanged
-            }
-            .foregroundStyle(.blue)
-            .font(.largeTitle)
-            .tint(.orange)
-            
-            Circle()
-                .fill(.red)
-                .frame(width: sliderValue, height: sliderValue, alignment: .center)
-                .opacity(sliderValueChanged ? 1 : 0)
         }.padding()
     }
 }
